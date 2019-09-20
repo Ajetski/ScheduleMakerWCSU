cdad9afe3d215944cc9acfb6370a021d9eabf5cc
Adam Jeniski <45019515+Ajetski@users.noreply.github.com>
2019-09-20 09:48:13 -0400

    added comments

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,8 +1,10 @@
 
-
+/* this will only be used if we try to automate finding the indicies of each column we need info from
 bool val_in(unsigned int val, unsigned int list[], unsigned int length) {
 	for (unsigned int i = 0; i < length; i++) {
 		if (val == list[i]) return true;
 	}
 	return false;
-}
+}*/
+
+
