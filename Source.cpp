86e99366e1e9788b25bae493101f217ac68364db
Ajetski <ajeniski4@gmail.com>
2019-09-20 22:49:18 -0400

    update indexes test to include through 19

--- Source.cpp
+++ Source.cpp
@@ -61,8 +61,8 @@ int main() {
 	
 	//json << startJson(json)";
 
-	//vector<unsigned int> indexes{ 3, 4, 5, 8, 9, 17, 18 };
-	vector<size_t> indexes{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	//vector<size_t> indexes{ 3, 4, 5, 8, 9, 17, 18 };
+	vector<size_t> indexes{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 	
 
 	string line;
