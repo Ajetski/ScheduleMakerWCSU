4b66f291924a47c115f4a7632fb7cb91637bda0d
Ajetski <ajeniski4@gmail.com>
2019-09-20 23:12:00 -0400

    fix comment

--- Utilities.cpp
+++ Utilities.cpp
@@ -30,7 +30,7 @@ string daysJsonify(string days) {
 	string output = "";
 	int pos = 0;
 	// 1 for loop that goes through each char in list. if current char is equal to the curr iterator from dayVals,
-	// inc iterator and push true, else push false
+	// push true, else push false; at end of loop inc iterator
 	for (int j = 0; j < (days.length() - 1);j++) {
 		while (pos < days.length()) {
 			if (days[j] == 'M') {
