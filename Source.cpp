c634c7aa96e0de9f27dbfd4e076bc5848b44f4f2
Ajetski <ajeniski4@gmail.com>
2019-09-24 20:56:00 -0400

    update source.cpp to pass correct

--- Source.cpp
+++ Source.cpp
@@ -39,10 +39,13 @@ using std::ios;*/
 
 using namespace std;
 
+vector<string> vectorizeString(string line);
+bool isPhysical(vector<string> vec);
+
 int main() {
 
 	//get csv vals
-	
+
 	//19 columns
 
 	//Index mapping
@@ -53,16 +56,17 @@ int main() {
 	//(4) time at index 9
 	//(5) location at index 17
 	//(6) instructors at index 18
+	//(7) color
+
+	vector<string> colors{ "#8DC028", "#2AFFE2", "#A1380F", "#3216EB", "#CC673E", "#53E29A", "#384E32", "#95D711", "#4EB3B1", "#3A7ECE"};
+	vector<string>::iterator colorIter = colors.begin();
+
+	cout << "Please input the name of a professor:\n>Sean Murthy\n";
+	string prof("Sean Murthy");
 
 	ofstream json; //a file that holds json data
 	json.open("output.csmo");
-	
 
-	vector<size_t> indexes{ 3, 4, 5, 8, 9, 17, 18 };
-	//vector<size_t> indexes{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-	
-
-	string line;
 
 	ifstream inFile;
 	inFile.open("Fall2019OpenClose.csv");
@@ -73,94 +77,115 @@ int main() {
 		exit(1); // terminate with error
 	}
 
-	vector<vector<string>> classList;
+	vector<vector<string>> classVec;
+	string line;
 
-	while (getline(inFile, line)) {
-		//reads from the file and converts csv data into a readable format
-		vector<string> vec;
-
-		// token string into vec
-		vector<size_t>::iterator curr = indexes.begin();
-		size_t last = 0;
-		size_t found = line.find(",");
-		size_t start_quote = line.find("\"", last);
-		
-
-		bool reached_end = false;
-		for (size_t i = 0; found != string::npos || ! reached_end; i++) {
-			if (found == string::npos && last != string::npos) {
-				//handle last cell in a line
-				string temp = line.substr(last, line.size() - last);
-				start_quote = temp.find("\"");
-				if (start_quote == string::npos && line.size() > last) {
-					//there no quotes
-					vec.push_back(temp);
+	if (getline(inFile, line)) {
+		vector<string> top;
+		while (getline(inFile, line)) {
+			vector<string> curr = vectorizeString(line);
+			if (!top.empty()) {
+				if (curr[0].size() < 1) {
+					//curr is a second instance of the same class as top
+					if (classVec.empty()) {
+						classVec.push_back(top);
+						classVec.push_back(curr);
+						top.clear();
+					}
+					else {
+						classVec.push_back(curr);
+					}
 				}
-				reached_end = true;
-			}
-			else if (start_quote < found) {
-				// in a string
-				size_t end_quote = line.find("\"", start_quote + 1);
-				string temp = line.substr(start_quote + 1, end_quote - start_quote - 1);
-				if (curr != indexes.end() && i == *curr) {
-					vec.push_back(line.substr(start_quote + 1, end_quote - start_quote - 1));
-					curr++;
+				else if (colorIter != colors.end() && classVec.size() >= 1) {
+					//curr is not a second instance of the same class as top
+					//json << jsonifyMeetings();
+					classVec[0].push_back(*(colorIter++));
+					cout << jsonifyMeeting(classVec, prof);
+					classVec.clear();
 				}
-				last = end_quote + 1;
-				start_quote = line.find("\"", end_quote + 1);
-				found = line.find(",", last + 1);
+				else if (colorIter != colors.end()) {
+					//curr is not a second instance of the same class as top
+					//json << jsonifyMeetings();
+					classVec.push_back(top);
+					classVec[0].push_back(*(colorIter++));
+					cout << jsonifyMeeting(classVec, prof);
+					classVec.clear();
+				}
 			}
 			else {
-				// not in a string
-				string temp = line.substr(last, found - last);
-				if (curr != indexes.end() && i == *curr) {
-					vec.push_back(line.substr(last, found - last));
-					curr++;
+				if (!classVec.empty() && colorIter != colors.end()) {
+					//json << jsonifyMeetings();
+					classVec[0].push_back(*(colorIter++));
+					cout << jsonifyMeeting(classVec, prof);
+					classVec.clear();
 				}
-				last = found + 1;
-				found = line.find(",", last);
+				if (curr[6].find(prof) != string::npos && isPhysical(curr)) {
+					top = curr;
+					classVec.push_back(top);
+				}
+				else {
+					top.clear();
+					curr.clear();
+				}
 			}
 		}
-
-		cout << vec.size() << "\n";
-
-		//print out tokens from this row
-
-		/*
-		for (vector<string>::iterator i = vec.begin(); i != vec.end(); i++) {
-			cout << "'" << *i << "' ";
-		}
-		cout << endl;
-		*/
-
-		if (vec[3].compare("") != 0) {
-			classList.push_back(vec);
-		}
 	}
-
-	
-
 	inFile.close();
-
-	string instructorName="";
-	//json << startJson();
-	for (vector<string> cls : classList) {
-		vector<string> teachers;
-		size_t primary = cls[6].find("(P)");
-		size_t last = 0;
-		while (primary != string::npos) {
-			teachers.push_back(cls[6].substr(last, primary - last));
-			last = cls[6].find(",",last);
-			primary = cls[6].find(",", last + 1);
-		}
-
-	}
-	//json << endJson();
 	json.close();
-
-	//cout << daysJsonify("MWF");
-	//cout << timeJsonify("05:30 am-08:30 pm");
-
-	return 0;
 }
 
+
+//returns a vecotr of data from a string
+vector<string> vectorizeString(string line){
+	vector<size_t> indexes{ 3, 4, 5, 8, 9, 17, 18 };
+	vector<string> vec;
+	vector<size_t>::iterator curr = indexes.begin();
+	size_t last = 0;
+	size_t found = line.find(",");
+	size_t start_quote = line.find("\"", last);
+	bool reached_end = false;
+	for (size_t i = 0; found != string::npos || !reached_end; i++) {
+		if (found == string::npos && last != string::npos) {
+			//handle last cell in a line
+			string temp = line.substr(last, line.size() - last);
+			start_quote = temp.find("\"");
+			if (start_quote == string::npos && line.size() > last) {
+				//there no quotes
+				vec.push_back(temp);
+			}
+			reached_end = true;
+		}
+		else if (start_quote < found) {
+			// in a string
+			size_t end_quote = line.find("\"", start_quote + 1);
+			string temp = line.substr(start_quote + 1, end_quote - start_quote - 1);
+			if (curr != indexes.end() && i == *curr) {
+				vec.push_back(line.substr(start_quote + 1, end_quote - start_quote - 1));
+				curr++;
+			}
+			last = end_quote + 1;
+			start_quote = line.find("\"", end_quote + 1);
+			found = line.find(",", last + 1);
+		}
+		else {
+			// not in a string
+			string temp = line.substr(last, found - last);
+			if (curr != indexes.end() && i == *curr) {
+				vec.push_back(line.substr(last, found - last));
+				curr++;
+			}
+			last = found + 1;
+			found = line.find(",", last);
+		}
+	}
+	return vec;
+}
+
+
+//returns a bool of if the locaiton string is not "ONLINE" or "HYBRID"
+bool isPhysical(vector<string> vec) {
+	if (vec[5].find("ONLINE") == string::npos && vec[5].find("HYBRID") == string::npos) {
+		return true;
+	}
+	return false;
+}
