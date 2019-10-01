b2831333ccb0d94767f20f8c4de85b61e4853d59
Ajetski <ajeniski4@gmail.com>
2019-10-01 18:53:44 -0400

    move functions to utilities.cpp
    also moved the using statements to utilities.h

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,10 +1,5 @@
 #include "Utilities.h"
 
-
-using std::string;
-using std::vector;
-
-
 string startJson(string prof) {
 	string title = ("\"Schedule for " + prof +  "\"");
 	return string("{\n\"dataCheck\": \"69761aa6-de4c-4013-b455-eb2a91fb2b76\",\n\"saveVersion\" : 4,\n\"schedules\" : [{\n\"title\": " + title + ",\n\"items\" : [");
@@ -125,51 +120,163 @@ string daysJsonify(string input) {
 	return output;
 }
 
-/*
-	Scratch paper
-
-	//input in form [subjectHead,courseNum,sectionNum,days(up to MTWRFS),times(hh:mm am/pm-hh:mm am/pm),[primProf (P), nextProf]]
-
-	this chunk is generated once per class section
-	{\n
-		\"uid\": \"79c8fe46-035e-4579-b2d8-5f1c2b96f3a0\",\n
-		\"type\": \"Course\",\n
-		\"title\": \"CS 205\",\n
-		\"meetingTimes\": [\n
 
 
+void parseData(ifstream& inFile, ofstream& output, string& prof) {
+	vector<string> colors{ "#8DC028", "#2AFFE2", "#A1380F", "#CC673E", "#53E29A",
+		"#95D711", "#4EB3B1", "#103D5B", "#3A7ECE", "#BD4433", "#E16D5C", "#933AB2", "#A78335",
+		"#ED5471", "#6DC81E", "#7DC4FE", "#3C7AA0", "#3216EB", "#384E32", "#A78335" };
 
-			This chunk is generated for each unique timeslot
+	//sectionTable holds all of the rows that descirbe a given secion. It is then used to jsonify all of the instances of that class
+	vector<vector<string>> sectionTable;
 
-			{\n
-				\"uid\": \"866e9185-4a29-47ae-a4b0-0423e633c8be\",\n
-				\"courseType\": \"\",\n
-				\"instructor\": \"\",\n
-				\"location\": \"\",\n
+	//line holds 1 line of the csv file at a time
+	string line;
 
-				\"startHour\": ,\n
-				\"endHour\": ,\n
-				\"startMinute\": ,\n
-				\"endMinute\": ,\n
-
-				\"days\": {\n
-				\"monday\": true,\n
-				\"tuesday\": false,\n
-				\"wednesday\": false,\n
-				\"thursday\": true,\n
-				\"friday\": false,\n
-				\"saturday\": false,\n
-				\"sunday\": false\n
-
-				}\n
-			}\n
+	//top is used to check and see if there are multiple instances of the same class
+	vector<string> top;
+	//first will be true until after the first meeting is added to the output stringstream
+	bool first = true;
 
 
-	This chunk is generated after all meeting times for a course are read
-	will be generated in colorJsonify
+	while (getline(inFile, line)) {
+		vector<string> curr = splitRow(line);
 
-		],\n
-		\"backgroundColor\": \"#FFE37D\"\n
-	}\n
-}\n
-*/
+		//for debugging
+		/*for (const string& s: curr)
+			cout << "'" << s << "' ";
+		cout << endl;*/
+
+		if (!top.empty()) {
+			if (curr[SUBJECT].empty()) {
+				//curr is a second instance of the same class as top
+				if (sectionTable.empty()) {
+					if (isPhysical(top))
+						//if top is a real class, we should add it to sectionTable
+						sectionTable.push_back(top);
+					if (isPhysical(curr))
+						//if curr is a real class, we should add it to calssVec
+						sectionTable.push_back(curr);
+					//if top is valid, it is now in sectionTable, and else it is not
+					//we should now clear top, as it is defined outside of this scope and will persist. (curr is defined in this loop)
+					top.clear();
+				}
+				else
+					//sectionTable is empty and curr is a second instance of the class in top
+					if (isPhysical(curr))
+						sectionTable.push_back(curr);
+			}
+			else if (!colors.empty() && !sectionTable.empty()) {
+				//curr is not a second instance of the same class as top
+				sectionTable[0].push_back(colors.back());
+				colors.pop_back();
+				if (!first)
+					output << ",\n";
+				first = false;
+				output << jsonifyMeeting(sectionTable, prof);
+				sectionTable.clear();
+				top.clear();
+				if (curr[6].find(prof) != string::npos && isPhysical(curr)) {
+					top = curr;
+					sectionTable.push_back(top);
+				}
+			}
+			else if (!colors.empty() && isPhysical(top)) {
+				//curr is not a second instance of the same class as top
+				sectionTable.push_back(top);
+				sectionTable[0].push_back(colors.back());
+				colors.pop_back();
+				if (!first)
+					output << ",\n";
+				first = false;
+				output << jsonifyMeeting(sectionTable, prof);
+				sectionTable.clear();
+				top.clear();
+			}
+		}
+		else {
+			if (!sectionTable.empty() && !colors.empty()) {
+				sectionTable[0].push_back(colors.back());
+				colors.pop_back();
+				if (!first)
+					output << ",\n";
+				first = false;
+				output << jsonifyMeeting(sectionTable, prof);
+				sectionTable.clear();
+				top.clear();
+			}
+			if (curr[6].find(prof) != string::npos && isPhysical(curr)) {
+				top = curr;
+				sectionTable.push_back(top);
+			}
+			else {
+				top.clear();
+				curr.clear();
+			}
+		}
+	}
+}
+
+
+//returns a vector of data from a string
+vector<string> splitRow(string line) {
+	vector<CSV_Index> indexes{ CSV_SUBJECT, CSV_COURSE, CSV_SECTION, CSV_DAYS, CSV_TIME, CSV_LOCATION, CSV_INSTRUCTOR };
+	vector<string> vec;
+	string::size_type last{ 0 };
+	string::size_type found{ line.find(",") };
+	string::size_type start_quote{ line.find("\"", last) };
+	bool reached_end = false;
+	for (string::size_type i{ 0 }; found != string::npos || !reached_end; i++) {
+		if (found == string::npos && last != string::npos) {
+			//handle last cell in a line
+			string temp(line.substr(last, line.size() - last));
+			start_quote = temp.find("\"");
+			if (start_quote == string::npos && line.size() > last)
+				//there no quotes
+				vec.push_back(temp);
+			reached_end = true;
+		}
+		else if (start_quote < found) {
+			// in a string
+			string::size_type end_quote = line.find("\"", start_quote + 1);
+			string temp = line.substr(start_quote + 1, end_quote - start_quote - 1);
+			if (!indexes.empty() && i == indexes.front()) {
+				vec.push_back(line.substr(start_quote + 1, end_quote - start_quote - 1));
+				indexes.erase(indexes.begin());
+			}
+			last = end_quote + 1;
+			start_quote = line.find("\"", end_quote + 1);
+			found = line.find(",", last + 1);
+		}
+		else {
+			// not in a string
+			string temp = line.substr(last, found - last);
+			if (!indexes.empty() && i == indexes.front()) {
+				vec.push_back(line.substr(last, found - last));
+				indexes.erase(indexes.begin());
+			}
+			last = found + 1;
+			found = line.find(",", last);
+		}
+	}
+	return vec;
+}
+
+
+//returns a bool of if the locaiton string is not "ONLINE" or "HYBRID"
+bool isPhysical(vector<string> vec) {
+	if (vec[5].find("ONLINE") == string::npos && vec[5].find("HYBRID") == string::npos)
+		return true;
+	return false;
+}
+
+//returns a bool expression for if a string is a valid name
+bool validName(string str) {
+	bool validFlag = true;
+	if (str.length() == 0)
+		validFlag = false;
+	for (char chr : str)
+		if ((chr < 'A' || chr > 'Z') && (chr < 'a' || chr > 'z') && (chr != '.') && (chr != ' ') && (chr != '-'))
+			validFlag = false;
+	return validFlag;
+}
--- src/utilities.h
+++ src/utilities.h
@@ -2,14 +2,28 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+
 using std::string;
 using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+using std::ios;
+using std::stringstream;
+using std::strlen;
 
 string startJson(string prof);
 string endJson();
 string daysJsonify(string weekdays);
 string timeJsonify(string time);
 string jsonifyMeeting(vector<vector<string>> data, string prof);
+vector<string> splitRow(string line);
+bool isPhysical(vector<string> vec);
+bool validName(string str);
+void parseData(ifstream& inFile, ofstream& output, string& prof);
 
 enum CSV_Index { CSV_SUBJECT = 3, CSV_COURSE = 4, CSV_SECTION = 5, CSV_DAYS = 8, CSV_TIME = 9, CSV_LOCATION = 17, CSV_INSTRUCTOR = 18 };
 enum Vec_Index { SUBJECT = 0, COURSE = 1, SECTION = 2, DAYS = 3, TIME = 4, LOCATION = 5, INSTRUCTOR = 6, COLOR = 7 };
